ba6550ddaa7747ed92ff9c33f6531fc991c7ef37
Tsierenana Botramanagna Gracy <gtsierenana@gmail.com>
2024-09-09 07:45:33 +0300

    try something

--- src/utils/getServerUrl.ts
+++ src/utils/getServerUrl.ts
@@ -1,14 +1,15 @@
 export const getServerUrl = () => {
-  const VITE_VERCEL_URL = import.meta.env.VITE_VERCEL_URL;
-  const VITE_APP_URL = import.meta.env.VITE_APP_URL;
+  const url = import.meta.env.VITE_VERCEL_URL
+  console.log(url);
+  return `https://${url}`
+  // if (VERCEL_URL) {
+  //   return `http://${VITE_VERCEL_URL}`;
+  // }
 
-  if (VITE_VERCEL_URL) {
-    return `http://${VITE_VERCEL_URL}`;
-  }
 
-  if (VITE_APP_URL) {
-    return VITE_APP_URL;
-  } else {
-    return "http://localhost:5173";
-  }
+  // if (VITE_APP_URL) {
+  //   return VITE_APP_URL;
+  // } else {
+  //   return "http://localhost:5173";
+  // }
 };
